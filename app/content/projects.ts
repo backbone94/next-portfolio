176b09aa367737131c9a545540f791cfa0e0c40e
leejeongjun <backbone94@naver.com>
2024-09-20 00:26:42 +0900

    chore: Figma to Map Plugin 프로젝트 위치 변경

--- app/content/projects.ts
+++ app/content/projects.ts
@@ -39,26 +39,6 @@ export const projects = [
     사용자가 작성한 일기 내용으로부터 감정을 분석하기 위해서 Naver CLOVA API를 사용했지만 해당 API는 감정을 (긍정, 부정, 중립) 이렇게 세 단계로만 분석해주는 한계가 있었습니다. 그래서 일기 내용에서 가장 많이 등장하는 단어들에게 가중치 부여한 후 나열해서, 관련된 내용의 가사를 가지는 음악을 추천하는 방식을 시도해보았으나, 단순히 등장하는 단어의 개수로 음악을 추천받기에는 뜬금 없는 음악을 추천해주고 있었습니다. 비용이 들긴 하지만 어쩔 수 없이 일기 내용과 관련된 음악을 추천받기에는 OpenAI의 API를 사용하는 것이 가장 적합할 것 같다는 생각이 들어서 OpenAI API를 사용하여 음악 추천 및 약 일주일 간의 일기 내용을 장문으로 정리해주는 기능을 추가하였습니다. 첨언하자면, 일주일 간의 일기 내용은 생각보다 많은 문자를 포함하고 있어서, 한 번의 API 호출로 0.15달러 이상이 소비되는 것을 보고, 이 어플리케이션은 상업적인 한계점을 가지고 있다고 생각하였습니다.<br>또한 Tailwind CSS 유틸리티 클래스를 활용하여 반응형 디자인을 구현한 상태라서, <a href="https://github.com/backbone94/music-recommendation-mobile" target="_blank" class="text-blue-500">React Native의 WebView</a>를 사용하면 웹 기반의 앱을 스마트폰의 앱으로 배포할 수 있는 것을 알게 되어 실제로 앱 배포를 하려고 했으나, 구글의 플레이스토어에서 개발자 계정을 심사받는 과정이 생각보다 까다롭다는 사실을 알게되었습니다. 계정 심사를 모두 받은 이후에는 다시 한 번 앱 배포를 시도해보고자 합니다.`,
   },
   {
-    title: 'Figma to Map Plugin',
-    media: [
-      { type: 'image', src: '/images/figma-plugin/main.png', alt: 'main' },
-    ],
-    features: '지도상에 Figma 컴포넌트 표시, 컴포넌트 드래그/리사이즈/회전, Figma <-> plugin 사이의 Import/Export, 지도 검색 기능 등',
-    stack: 'Figma Plugin API, TypeScript, React, Webpack, MUI, OpenLayers',
-    github: '',
-    description: `<strong>개요</strong><br>
-    인턴으로서 참여한 첫 프로젝트입니다. 프로젝트의 목표는 디자이너와 개발자 간의 협업을 지원하는 Figma에서, 특정 컴포넌트를 지도상의 특정 위치에 배치하고, 해당 위치의 위경도 좌표를 기억할 수 있는 Figma 플러그인을 개발하는 것이었습니다.<br><br>
-    <strong>배경</strong><br>
-    Figma 플러그인의 기본 뼈대는 Figma Plugin API의 <a href="https://www.figma.com/plugin-docs/" target="_blank" class="text-blue-500">공식 문서</a>를 참고하여 구성하였으며, 코드 유지보수성을 향상시키고, 바닐라 JavaScript보다 훨씬 더 빨리 작업 속도를 낼 수 있는 React를, 그리고 타입 체크를 통해 더욱 안정적인 개발 진행을 위한 TypeScript를 선택하였습니다.<br>
-    하지만 React를 TypeScript와 사용하기 위해서는 Webpack과 같은 번들러의 복잡한 설정이 필요하였습니다. 이런 상황에서 좀 더 효율적인 Figma 플러그인 개발을 위한 방법을 검색해보다가, Figma와 (Webpack + TypeScript + React) 간의 원활한 조화를 위해 이미 개발되어 있던 <a href="https://github.com/hseoy/figma-plugin-react-boilerplate" target="_blank" class="text-blue-500">보일러플레이트</a>를 발견하게 되었습니다. 이 보일러플레이트를 사용하여, 따로 번거로운 설정 없이 React와 TypeScript 스펙을 Figma 플러그인 개발에 사용할 수 있어서 편리한 작업을 할 수 있었습니다.<br>
-    UI 라이브러리는, 구글의 Material Design 가이드라인을 바탕으로 만들어진 MUI(Material UI) 라이브러리를 활용하여 React의 컴포넌트와 잘 어우러지도록 UI를 개선하였습니다.<br><br>
-    <strong>고민의 흔적</strong><br>
-    Figma 플러그인 위에 지도를 표시하기 위해, 초반에는 Leaflet 라이브러리를 사용하여 개발 진행을 하고 있었으나, Leaflet 라이브러리는, 지도 위에 Polygon을 그리는 기능과, Polygon의 모양 변형, 드래그, 리사이즈 등의 기능을 구현하기 위한 커스텀을 제공하지 않는 고도화된 지도 라이브러리가 아니었기에, 사수분과의 상담를 통해 Leaflet 라이브러리보다 더 다양한 커스텀 기능을 제공하는 OpenLayers 라이브러리로 대체하는 방식으로 수정하여 지도 구현을 하였습니다.<br>
-    또한 Polygon을 지도의 특정 위치에 배치시켜야 할 때, 지도를 직접 드래그 하면서 그 지역을 찾으려고 하는 게 매우 비효율적이다라는 생각을 하게 되었고, 인턴 프로젝트로써 예정에 없었던 지도 검색 기능을 Nominatim 라이브러리를 사용하여 추가하였습니다.<br>
-    이 플러그인의 핵심 기능인, Polygon의 위경도 좌표 변환을 위해서는 좌표계에 대한 개념을 알아야 했는데, OpenLayers 라이브러리가 사용하고 있는 EPSG:3857 좌표계와, 위경도 좌표를 나타내는 데에 사용되는 EPSG:4326 좌표계와 같은 생소한 개념들을 알지 못한 채로 작업을 진행해서 중간에 좌표 변환이 제대로 이뤄지지 않는 바람에 조금 힘들었던 경험을 하였습니다.<br>
-    지도 라이브러리의 첫 사용과, 좌표계에 대한 새로운 개념을 공부할 수 있었던 뜻깊은 프로젝트였습니다.`,
-  },
-  {
     title: '살롱 | SSALON',
     media: [
       { type: 'image', src: '/images/ssalon/ticket.png', alt: 'ticket' },
@@ -185,5 +165,25 @@ export const projects = [
     
     3. 다크모드 기능: 사용자 접근성과 편의성을 높이기 위해 다크모드 기능을 구현했습니다. SCSS를 통해 사용자의 선호에 따라 다크모드와 라이트모드를 전환할 수 있도록 설계하였습니다.<br><br>
   `,
+  },
+  {
+    title: 'Figma to Map Plugin',
+    media: [
+      { type: 'image', src: '/images/figma-plugin/main.png', alt: 'main' },
+    ],
+    features: '지도상에 Figma 컴포넌트 표시, 컴포넌트 드래그/리사이즈/회전, Figma <-> plugin 사이의 Import/Export, 지도 검색 기능 등',
+    stack: 'Figma Plugin API, TypeScript, React, Webpack, MUI, OpenLayers',
+    github: '',
+    description: `<strong>개요</strong><br>
+    인턴으로서 참여한 첫 프로젝트입니다. 프로젝트의 목표는 디자이너와 개발자 간의 협업을 지원하는 Figma에서, 특정 컴포넌트를 지도상의 특정 위치에 배치하고, 해당 위치의 위경도 좌표를 기억할 수 있는 Figma 플러그인을 개발하는 것이었습니다.<br><br>
+    <strong>배경</strong><br>
+    Figma 플러그인의 기본 뼈대는 Figma Plugin API의 <a href="https://www.figma.com/plugin-docs/" target="_blank" class="text-blue-500">공식 문서</a>를 참고하여 구성하였으며, 코드 유지보수성을 향상시키고, 바닐라 JavaScript보다 훨씬 더 빨리 작업 속도를 낼 수 있는 React를, 그리고 타입 체크를 통해 더욱 안정적인 개발 진행을 위한 TypeScript를 선택하였습니다.<br>
+    하지만 React를 TypeScript와 사용하기 위해서는 Webpack과 같은 번들러의 복잡한 설정이 필요하였습니다. 이런 상황에서 좀 더 효율적인 Figma 플러그인 개발을 위한 방법을 검색해보다가, Figma와 (Webpack + TypeScript + React) 간의 원활한 조화를 위해 이미 개발되어 있던 <a href="https://github.com/hseoy/figma-plugin-react-boilerplate" target="_blank" class="text-blue-500">보일러플레이트</a>를 발견하게 되었습니다. 이 보일러플레이트를 사용하여, 따로 번거로운 설정 없이 React와 TypeScript 스펙을 Figma 플러그인 개발에 사용할 수 있어서 편리한 작업을 할 수 있었습니다.<br>
+    UI 라이브러리는, 구글의 Material Design 가이드라인을 바탕으로 만들어진 MUI(Material UI) 라이브러리를 활용하여 React의 컴포넌트와 잘 어우러지도록 UI를 개선하였습니다.<br><br>
+    <strong>고민의 흔적</strong><br>
+    Figma 플러그인 위에 지도를 표시하기 위해, 초반에는 Leaflet 라이브러리를 사용하여 개발 진행을 하고 있었으나, Leaflet 라이브러리는, 지도 위에 Polygon을 그리는 기능과, Polygon의 모양 변형, 드래그, 리사이즈 등의 기능을 구현하기 위한 커스텀을 제공하지 않는 고도화된 지도 라이브러리가 아니었기에, 사수분과의 상담를 통해 Leaflet 라이브러리보다 더 다양한 커스텀 기능을 제공하는 OpenLayers 라이브러리로 대체하는 방식으로 수정하여 지도 구현을 하였습니다.<br>
+    또한 Polygon을 지도의 특정 위치에 배치시켜야 할 때, 지도를 직접 드래그 하면서 그 지역을 찾으려고 하는 게 매우 비효율적이다라는 생각을 하게 되었고, 인턴 프로젝트로써 예정에 없었던 지도 검색 기능을 Nominatim 라이브러리를 사용하여 추가하였습니다.<br>
+    이 플러그인의 핵심 기능인, Polygon의 위경도 좌표 변환을 위해서는 좌표계에 대한 개념을 알아야 했는데, OpenLayers 라이브러리가 사용하고 있는 EPSG:3857 좌표계와, 위경도 좌표를 나타내는 데에 사용되는 EPSG:4326 좌표계와 같은 생소한 개념들을 알지 못한 채로 작업을 진행해서 중간에 좌표 변환이 제대로 이뤄지지 않는 바람에 조금 힘들었던 경험을 하였습니다.<br>
+    지도 라이브러리의 첫 사용과, 좌표계에 대한 새로운 개념을 공부할 수 있었던 뜻깊은 프로젝트였습니다.`,
   },
 ];
