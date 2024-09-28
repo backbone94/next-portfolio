dbf489bd276d3c6ac43b35058afd08e8d27b10ff
leejeongjun <backbone94@naver.com>
2024-09-28 17:49:23 +0900

    fix: 영상 control이 클릭이 되지 않는 문제 수정
    - pagination과의 충돌로 인하여 클릭이 안되고 있었음
    - pagination을 제거
    - 추가로 스와이프의 loop를 제거

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -24,11 +24,6 @@ export default function Project() {
     Swiper.use([Navigation, Pagination]);
 
     new Swiper('.swiper', {
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -98,7 +93,6 @@ export default function Project() {
                       </div>
                     ))}
                   </div>
-                  <div className="swiper-pagination"></div>
                   <div className="swiper-button-prev"></div>
                   <div className="swiper-button-next"></div>
                 </div>
