935b182344dfe880efd764e63a5f5c4f27c00978
leejeongjun <backbone94@naver.com>
2026-04-20 15:40:52 +0900

    chore: 푸터 Right 연도 수정

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="w-full py-4 bg-gray-800 text-white">
       <div className="container mx-auto text-center">
-        <span className="text-sm">Made By Jun 2024. All Rights Reserved.</span>
+        <span className="text-sm">Made By Jun 2026. All Rights Reserved.</span>
       </div>
     </footer>
   );
