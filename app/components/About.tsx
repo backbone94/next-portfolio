553d26a515828d760e063f7d399c8b03ec0f9ec3
leejeongjun <backbone94@naver.com>
2026-04-20 16:42:18 +0900

    chore: About 수정

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -24,14 +24,14 @@ export default function About() {
           />
         </a>
         <div className="text-center text-lg font-semibold mb-16 text-gray-600 leading-relaxed">
-          도전과 성취를 향해 끊임없이 전진하는 개발자,<br />이정준입니다.
+          더 나은 개발을 위해 오늘도 1일 1커밋,<br />풀스택 개발자 이정준입니다.
         </div>
 
         <div className="space-y-4 w-full">
           <div className="flex items-start">
             <span className="mr-4 text-xl">🌱</span>
             <p className="text-base text-gray-800 leading-normal">
-              웹 프론트엔드 개발자로서의 경력을 가지고 있으며, 무엇보다 사용자의 경험을 우선시 합니다.
+              1년 9개월 간의 프론트엔드 개발자로서의 경력을 가지고 있으며, 무엇보다 사용자의 경험을 우선시 합니다.
             </p>
           </div>
 
